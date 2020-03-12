b6183b1921144e5167639a109c04e1abbed51c8f
leandrocodes <leandrovianacodes@gmail.com>
2020-03-12 14:32:36 -0400

    Applying the Function

--- genPopulation.py
+++ genPopulation.py
@@ -1,4 +1,5 @@
 import random
+import math
 
 def generate_population(size, x_bound, y_bound):
   lower_x_bound, upper_x_bound = x_bound
@@ -6,7 +7,7 @@ def generate_population(size, x_bound, y_bound):
 
   population = []
 
-  for i in range(rize):
+  for i in range(size):
     ind = {
       "x": random.uniform(lower_x_bound, upper_x_bound),
       "y": random.uniform(lower_y_bound, upper_y_bound)
@@ -14,4 +15,27 @@ def generate_population(size, x_bound, y_bound):
     population.append(ind)
   
   return population
+
+def apply_function(ind):
+  x =  ind["x"]
+  y = ind["y"]
+  return math.sin(math.sqrt(x ** 2 + y ** 2))
+
+
+
+generations = 100
+
+population = generate_population(size = 10, x_bound = (-4, 4), y_bound = (-4, 4))
       
+i = 1
+
+while True:
+  print(f"🧬 GENERATION {i}")
+
+  for ind in population:
+    print(ind)
+
+  if i == generations:
+    break
+  
+  i += 1
