ea1998a4df82bd40e6e0d0f6d0741bb1bca2a8aa
leandrocodes <leandrovianacodes@gmail.com>
2020-03-24 18:30:05 -0400

    encoding data

--- tsp.py
+++ tsp.py
@@ -1,5 +1,9 @@
+import json
+
+
 def generate(cities):
     return True
+
 
 def fitness(city):
     return 1
@@ -16,5 +20,27 @@ def mutate(city):
 def main(iterations):
     return True
 
+
 if __name__ == "__main__":
-    pass
+    json_file = open("graph.json", encoding="utf8")
+    graph = json.load(json_file)
+
+    graph_encoded = {}
+    edges_enconded = {}
+
+    for key in graph:
+        if (key == "nodes"):
+            # print(graph[key])
+            count = 0
+            for node in graph[key]:
+                graph_encoded[node] = count
+                count += 1
+
+    for key in graph:
+        if (key == "edges"):
+            for edge in graph[key]:
+                edges_enconded[graph_encoded[edge[0]], graph_encoded[edge[1]]] =  edge[2]
+
+
+    # print(json.dumps(graph_encoded, indent=4, ensure_ascii=False))
+    print(edges_enconded)
