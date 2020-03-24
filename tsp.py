2620b0b0636b378deb8d8bdccd4194704b471b91
leandrocodes <leandrovianacodes@gmail.com>
2020-03-24 18:03:21 -0400

    Usando JSON para facilitar

--- tsp.py
+++ tsp.py
@@ -1,24 +1,20 @@
-cities = [
-    [0, 325, 420, 230, 143, 189],
-    [325, 0, 335, 270, 285, 474],
-    [420, 335, 0, 588, 563, 609],
-    [230, 270, 588, 0, 373, 413],
-    [143, 285, 563, 373, 0, 213],
-    [189, 474, 609, 413, 213, 0]
-
-]
-
-for city in cities:
-    print(city)
+def generate(cities):
+    return True
 
 def fitness(city):
     return 1
 
+
 def crossover(a, b):
     return a
+
 
 def mutate(city):
     return city
 
+
 def main(iterations):
     return True
+
+if __name__ == "__main__":
+    pass
